a435219bfd09b172a869a37d6972e02b534d855f
stone <shilei_zhang@163.com>
2023-10-25 18:27:54 +0800

    fix(main): 修复模板存在重复代码块、多空行时，代理断点映射行错误的问题

--- src/main/kotlin/io/maliboot/www/hyperf/common/extend/LighterASTExt.kt
+++ src/main/kotlin/io/maliboot/www/hyperf/common/extend/LighterASTExt.kt
@@ -5,6 +5,7 @@ import com.intellij.lang.LighterASTNode
 import com.intellij.psi.impl.source.tree.LightTreeUtil
 import com.intellij.psi.impl.source.tree.RecursiveLighterASTNodeWalkingVisitor
 import com.intellij.psi.tree.IElementType
+import com.intellij.ui.treeStructure.Tree
 import com.jetbrains.php.lang.lexer.PhpTokenTypes
 import com.jetbrains.php.lang.parser.PhpElementTypes
 
@@ -45,6 +46,69 @@ fun LighterAST.getMethodLightASTNode(methodName: String): LighterASTNode? {
     return resultNode
 }
 
+fun LighterAST.findFirstChildLightASTNode(
+    parentASTNode: LighterASTNode,
+    startOffset: Int
+): LighterASTNode? {
+    if (startOffset < parentASTNode.startOffset) {
+        return null
+    }
+
+    var resultNode: LighterASTNode? = null
+    object : RecursiveLighterASTNodeWalkingVisitor(this) {
+        override fun visitNode(element: LighterASTNode) {
+            if (element.startOffset > startOffset) {
+                resultNode = element
+                super.stopWalking()
+                return
+            }
+            super.visitNode(element)
+        }
+    }.visitNode(parentASTNode)
+    return resultNode
+}
+
+fun LighterAST.getTreeIndexPath(
+    parentASTNode: LighterASTNode,
+    childASTNode: LighterASTNode,
+    deepMax: Int = 500
+): MutableList<Int>? {
+    var count = deepMax
+    var myChildNode = childASTNode
+
+    val path: MutableList<Int> = mutableListOf()
+    while (count >= 0) {
+        val myParentNode = this.getParent(myChildNode) ?: return null
+        // 追加path index
+        path.add(0, this.getChildren(myParentNode).indexOf(myChildNode))
+        // 遍历完成
+        if (myParentNode == parentASTNode) {
+            break
+        }
+        // 下一轮
+        myChildNode = myParentNode
+        count--
+    }
+    if (path.isEmpty()) {
+        return null
+    }
+    return path
+}
+
+fun LighterAST.getChildByTreeIndexPath(parentASTNode: LighterASTNode, treeIndexPath: List<Int>): LighterASTNode? {
+    var currentIndex = 0
+    var myParentNode = parentASTNode
+
+    while (currentIndex < treeIndexPath.size) {
+        myParentNode = this.getChildren(myParentNode).takeIf {
+            it.size > treeIndexPath[currentIndex]
+        }?.get(treeIndexPath[currentIndex]) ?: return null
+        currentIndex++
+    }
+
+    return myParentNode
+}
+
 fun LighterAST.getChildrenOfType(node: LighterASTNode, type: IElementType): LighterASTNode? {
     return LightTreeUtil.getChildrenOfType(this, node, type)
         .takeIf { it.isNotEmpty() }
@@ -57,14 +121,24 @@ fun LighterAST.getNodeText(node: LighterASTNode, type: IElementType = PhpTokenTy
     }
 }
 
+fun LighterAST.getGroupStmtNode(parentASTNode: LighterASTNode): LighterASTNode? {
+    return getChildrenOfType(parentASTNode, PhpElementTypes.GROUP_STATEMENT)
+}
+
 fun LighterAST.getHyperfProxyClosure(methodNode: LighterASTNode): LighterASTNode? {
     getChildrenOfType(methodNode, PhpElementTypes.GROUP_STATEMENT)?.let { groupStmtNode->
         getChildrenOfType(groupStmtNode, PhpElementTypes.RETURN)?.let { groupStmtReturnNode ->
             getChildrenOfType(groupStmtReturnNode, PhpElementTypes.METHOD_REFERENCE)?.takeIf {
                 // 查找 hyperf -> self::__proxyCall
                 getNodeText(it) == "__proxyCall"
-            }?.let {
-                return it
+            }
+        }
+    }?.let{ selfProxyCall ->
+        getChildrenOfType(selfProxyCall, PhpElementTypes.PARAMETER_LIST)?.let { selfProxyCallParameterList ->
+            getChildrenOfType(selfProxyCallParameterList, PhpElementTypes.CLOSURE)?.let { proxyClosure ->
+                getChildrenOfType(proxyClosure, PhpElementTypes.FUNCTION)?.let {
+                    return it
+                }
             }
         }
     }
--- src/main/kotlin/io/maliboot/www/hyperf/debug/HyperfProxyPathMapper.kt
+++ src/main/kotlin/io/maliboot/www/hyperf/debug/HyperfProxyPathMapper.kt
@@ -9,7 +9,6 @@ import com.intellij.xdebugger.XSourcePosition
 import com.jetbrains.php.debug.template.PhpTemplateDebugStateService
 import com.jetbrains.php.debug.template.PhpTemplateLanguagePathMapper
 import com.jetbrains.php.lang.PhpFileType
-import com.jetbrains.php.lang.parser.PhpElementTypes
 import com.jetbrains.php.util.pathmapper.PhpPathMapper
 import io.maliboot.www.hyperf.common.extend.*
 
@@ -67,50 +66,59 @@ class HyperfProxyPathMapper : PhpTemplateLanguagePathMapper() {
         // 代理文件
         val proxyFile = originPos.file.getHyperfProxyFile(project) ?: return null
 
-        // 原文件-获取类方法GroupStmt的开始行
+        // 模板文件
         val originTree = originPos.file.findPsiFile(project)?.node?.lighterAST ?: return null
         val originMethodNode = originTree.getMethodLightASTNode(originPos.offset) ?: return null
         val classMethodName = originTree.getNodeText(originMethodNode) ?: return null
-        val originFuncStartLine = originTree
-            .getChildrenOfType(originMethodNode, PhpElementTypes.GROUP_STATEMENT)
-            ?.getStartLineNumber(originPos.file)
-            ?: return null
+        // 获取方法与断点之间的treePath
+        val sourcePosRelativeTreePath =
+            originTree.getGroupStmtNode(originMethodNode)?.let { originMethodGroupStmtNode ->
+                originTree.findFirstChildLightASTNode(originMethodGroupStmtNode, originPos.offset)?.let {
+                    originTree.getTreeIndexPath(originMethodGroupStmtNode, it)
+                }
+            } ?: return null
 
         // 代理文件位置
         val proxyTree = proxyFile.findPsiFile(project)?.node?.lighterAST ?: return null
         proxyTree.getMethodLightASTNode(classMethodName)?.let { proxyMethodNode ->
             // 排除AOP代码干扰
             proxyTree.getHyperfProxyClosure(proxyMethodNode) ?: proxyMethodNode
-        }?.getStartLineNumber(proxyFile)?.let {
-            it + (originPos.line - originFuncStartLine)
         }?.let {
-            return XDebuggerUtil.getInstance().createPosition(proxyFile, it)
+            proxyTree.getGroupStmtNode(it)
+        }?.let {
+            proxyTree.getChildByTreeIndexPath(it, sourcePosRelativeTreePath)
+        }?.let {
+            return XDebuggerUtil.getInstance().createPositionByOffset(proxyFile, it.startOffset)
         }
 
         return null
     }
 
     private fun guessOriginXSourcePosition(proxyPos: XSourcePosition, project: Project): XSourcePosition? {
-        // 获取原文件
+        // 获取模板
         val originFile = proxyPos.file.getHyperfOriginFile(project) ?: return null
 
         // 代理文件-获取类方法[代理闭包]GroupStmt的开始行
         val proxyTree = proxyPos.file.findPsiFile(project)?.node?.lighterAST ?: return null
         val proxyMethodNode = proxyTree.getMethodLightASTNode(proxyPos.offset) ?: return null
         val classMethodName = proxyTree.getNodeText(proxyMethodNode) ?: return null
-        val proxyFuncStartLine = proxyTree.getChildrenOfType(proxyMethodNode, PhpElementTypes.GROUP_STATEMENT)
-            ?.getStartLineNumber(proxyPos.file)?.let {
-                proxyTree.getHyperfProxyClosure(proxyMethodNode)?.getStartLineNumber(proxyPos.file) ?: it
-            } ?: return null
+        // 获取方法与断点之间的treePath
+        val sourcePosRelativeTreePath = (proxyTree.getHyperfProxyClosure(proxyMethodNode) ?: proxyMethodNode).let {
+            proxyTree.getGroupStmtNode(it)
+        }?.let { funcNode ->
+            proxyTree.findFirstChildLightASTNode(funcNode, proxyPos.offset)?.let {
+                proxyTree.getTreeIndexPath(funcNode, it)
+            }
+        } ?: return null
 
-        // 原文件位置
+        // 模板位置
         val originTree = originFile.findPsiFile(project)?.node?.lighterAST ?: return null
         originTree.getMethodLightASTNode(classMethodName)?.let {
-            originTree.getChildrenOfType(it, PhpElementTypes.GROUP_STATEMENT)
-        }?.getStartLineNumber(originFile)?.let {
-            it + (proxyPos.line - proxyFuncStartLine)
+            originTree.getGroupStmtNode(it)
         }?.let {
-            return XDebuggerUtil.getInstance().createPosition(originFile, it)
+            originTree.getChildByTreeIndexPath(it, sourcePosRelativeTreePath)
+        }?.let {
+            return XDebuggerUtil.getInstance().createPositionByOffset(originFile, it.startOffset)
         }
 
         return null
